441f1a1edcfb39339116b05194be485b9a1c6569
Avdieienko <avdieienko.d@gmail.com>
2024-10-04 23:37:37 +0100

    Clean up

--- Src/drive.c
+++ Src/drive.c
@@ -2,8 +2,4 @@
 #include "../Inc/CarSensors.h"
 #include "../Inc/drive.h"
 
-int drive(CarSensors* carSensorsPtr) {
-  // Step 1: Change sensor values
-
-  return 0;
-}
+// Step 1: Initialise drive function
